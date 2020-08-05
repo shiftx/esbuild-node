4dd75411fed7d1b628718e1e3940c6cbe3a9b7ec
Eigil Sagafos <eigil@sagafos.no>
2020-08-05 10:24:37 +0200

    Make esbuild a peer dependency. Remove use of tmp file in favor of in memory output

--- bin/esbuild-node.js
+++ bin/esbuild-node.js
@@ -13,21 +13,15 @@ if (!fs.existsSync("./" + script)) {
     throw new Error("No script found");
 }
 
-// Generate tmp build dir
-var path = require("path");
-var os = require("os");
-var tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "esbuild-node-"));
-
 // Build
 var buildSync = require("esbuild").buildSync;
-var outfile = tmpDir + "/out.js";
+
 const res = buildSync({
     entryPoints: ["./" + script],
-    outfile: outfile,
+    write: false,
     platform: "node",
     bundle: true,
     target: "es2015",
 });
 
-// Load built file
-require(outfile);
+eval(Buffer.from(res.outputFiles[0].contents).toString())
