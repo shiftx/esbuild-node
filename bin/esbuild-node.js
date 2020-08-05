d72aa2573fc069d04978541e1b0d4ab0eb07853c
Eigil Sagafos <eigil@sagafos.no>
2020-08-05 10:35:39 +0200

    Check for js, jsx, ts and tsx file if no extension provided

--- bin/esbuild-node.js
+++ bin/esbuild-node.js
@@ -9,6 +9,15 @@ process.argv = process.argv.filter(function(_, i) {
 
 // Check if script file exists
 var fs = require("fs");
+if (!script.match(/\.(ts|js)x?/)) {
+    ['ts', 'js', 'tsx', 'jsx'].find(ext => {
+        if (fs.existsSync("./" + script + '.' + ext)) {
+            script = script + '.' + ext
+            return true
+        }
+    })
+}
+
 if (!fs.existsSync("./" + script)) {
     throw new Error("No script found");
 }
@@ -16,7 +25,7 @@ if (!fs.existsSync("./" + script)) {
 // Build
 var buildSync = require("esbuild").buildSync;
 
-const res = buildSync({
+var res = buildSync({
     entryPoints: ["./" + script],
     write: false,
     platform: "node",
@@ -24,4 +33,4 @@ const res = buildSync({
     target: "es2015",
 });
 
-eval(Buffer.from(res.outputFiles[0].contents).toString())
+eval(Buffer.from(res.outputFiles[0].contents).toString());
