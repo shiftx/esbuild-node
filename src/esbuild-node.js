dadb210ad9845ee1c9346c5dd847a3f30e7a0cb7
Eigil Sagafos <eigil@sagafos.no>
2021-10-22 08:40:39 +0200

    Add files to packages.json

---
+++ src/esbuild-node.js
@@ -0,0 +1,63 @@
+// Get and remove script from argv
+
+var argv = process.argv;
+var script = argv[2];
+process.argv = process.argv.filter(function (_, i) {
+  return i !== 2;
+});
+
+if (!script) {
+  console.error("Error: No script provided");
+  process.exit(1);
+}
+
+// Check if script file exists
+var fs = require("fs");
+if (!script.match(/\.(ts|js)x?/)) {
+  ["ts", "js", "tsx", "jsx"].find(ext => {
+    if (fs.existsSync("./" + script + "." + ext)) {
+      script = script + "." + ext;
+      return true;
+    }
+  });
+}
+
+if (!fs.existsSync("./" + script)) {
+  throw new Error("No script found");
+}
+
+var cwd = process.cwd();
+__dirname = cwd;
+__filename = cwd + "/" + script;
+
+// Build
+var buildSync = require("esbuild").buildSync;
+
+var path = require("path");
+
+var res = buildSync({
+  entryPoints: [__filename],
+  write: false,
+  platform: "node",
+  bundle: true,
+  target: "node" + process.version.match(/v(.+)/)[1],
+});
+
+var Module = require("module");
+var dir = path.dirname(script);
+var scriptModule = new Module(dir);
+scriptModule.filename = script;
+scriptModule.paths = Module._nodeModulePaths(dir);
+
+Object.assign(global, {
+  __filename: script,
+  __dirname: dir,
+  exports: scriptModule.exports,
+  module: scriptModule,
+  require: scriptModule.require.bind(scriptModule),
+});
+
+require("vm").runInThisContext(
+  Buffer.from(res.outputFiles[0].contents).toString(),
+  { filename: script }
+);
