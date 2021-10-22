0fe911dd75b8fbb47a7f24deeb62ed50af7c05c9
Eigil Sagafos <eigil@sagafos.no>
2021-10-22 08:30:01 +0200

    Remove node_modules traversal for finding external modules for better compatability with yarn 3.0

--- bin/esbuild-node.js
+++ bin/esbuild-node.js
@@ -3,78 +3,62 @@
 // Get and remove script from argv
 var argv = process.argv;
 var script = argv[2];
-process.argv = process.argv.filter(function(_, i) {
-    return i !== 2;
+process.argv = process.argv.filter(function (_, i) {
+  return i !== 2;
 });
 
 if (!script) {
-    console.error("Error: No script provided")
-    process.exit(1)
+  console.error("Error: No script provided");
+  process.exit(1);
 }
 
 // Check if script file exists
 var fs = require("fs");
 if (!script.match(/\.(ts|js)x?/)) {
-    ['ts', 'js', 'tsx', 'jsx'].find(ext => {
-        if (fs.existsSync("./" + script + '.' + ext)) {
-            script = script + '.' + ext
-            return true
-        }
-    })
+  ["ts", "js", "tsx", "jsx"].find(ext => {
+    if (fs.existsSync("./" + script + "." + ext)) {
+      script = script + "." + ext;
+      return true;
+    }
+  });
 }
 
 if (!fs.existsSync("./" + script)) {
-    throw new Error("No script found");
+  throw new Error("No script found");
 }
 
-var cwd = process.cwd()
-__dirname = cwd
-__filename = cwd + '/' + script
+var cwd = process.cwd();
+__dirname = cwd;
+__filename = cwd + "/" + script;
 
 // Build
 var buildSync = require("esbuild").buildSync;
 
-// Find all node modules to mark as external
-var path = require('path');
-var external = [];
-function findNodeModules(dir) {
-    var moduleDir = path.join(dir, 'node_modules');
-    if (fs.existsSync(moduleDir)) {
-        var packages = fs.readdirSync(moduleDir);
-        packages.forEach(function (package) {
-            if (package.charAt(0) !== '.')
-                external.push(package);
-        });
-    }
-    var parent = path.dirname(dir);
-    if (parent !== dir && fs.existsSync(parent)) findNodeModules(parent);
-}
-findNodeModules(process.cwd());
+var path = require("path");
 
 var res = buildSync({
-    entryPoints: [__filename],
-    write: false,
-    platform: "node",
-    bundle: true,
-    target: "es2015",
-    external: external
+  entryPoints: [__filename],
+  write: false,
+  platform: "node",
+  bundle: true,
+  target: "node" + process.version.match(/v(.+)/)[1],
 });
 
-var Module = require('module');
+var Module = require("module");
 var dir = path.dirname(script);
 var scriptModule = new Module(dir);
 scriptModule.filename = script;
 scriptModule.paths = Module._nodeModulePaths(dir);
 
 Object.assign(global, {
-    __filename: script,
-    __dirname: dir,
-    exports: scriptModule.exports,
-    module: scriptModule,
-    require: scriptModule.require.bind(scriptModule)
+  __filename: script,
+  __dirname: dir,
+  exports: scriptModule.exports,
+  module: scriptModule,
+  require: scriptModule.require.bind(scriptModule),
 });
 
-require('vm').runInThisContext(
-    Buffer.from(res.outputFiles[0].contents).toString(),
-    {filename: script}
+require("vm").runInThisContext(
+  Buffer.from(res.outputFiles[0].contents).toString(),
+  { filename: script }
 );
